8ae209b94a2c393c800ba6aeb81268093641270d
dnyanu2006 <dnyaneshwariwalunj6@gmail.com>
2026-06-17 17:08:18 +0530

    fix: move vite to devDependencies and fix build

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
   "http://localhost:5173",
-  "https://your-frontend-url.onrender.com"
+  "https://hired-project-beta.vercel.app"
 ],
   credentials: true
 }));
