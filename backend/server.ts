351f58d31bf6315075ca125009cb76589cb58510
dnyanu2006 <dnyaneshwariwalunj6@gmail.com>
2026-06-21 11:26:52 +0530

    use direct vite build

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
   "http://localhost:5173",
-  "https://hired-project-beta.vercel.app"
+  "https://hired-project-beta.vercel.app",
+  "https://hired-project-pied.vercel.app",
 ],
   credentials: true
 }));
