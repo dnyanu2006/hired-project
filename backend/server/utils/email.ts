f9c6bd4c5445bbaccfd5fb6844e01b0fadbf9b65
dnyanu2006 <dnyaneshwariwalunj6@gmail.com>
2026-06-21 12:31:30 +0530

    use direct vite build

--- backend/server/utils/email.ts
+++ backend/server/utils/email.ts
@@ -1,20 +1,14 @@
-/*import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
 const getTransporter = () => {
   return nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
-    tls: {
-      rejectUnauthorized: false,
-    },
   });
 };
-
 export const sendEmail = async (to: string, subject: string, html: string) => {
   try {
     console.log("SMTP USER:", process.env.SMTP_USER);
@@ -32,26 +26,4 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
   } catch (error) {
     console.error("EMAIL FAILED FULL ERROR:", error);
   }
-};*/
-import nodemailer from "nodemailer";
-
-const getTransporter = () => {
-  return nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
-};
-
-export const sendEmail = async (to: string, subject: string, html: string) => {
-  const transporter = getTransporter();
-
-  await transporter.sendMail({
-    from: `"Job Portal" <${process.env.SMTP_USER}>`,
-    to,
-    subject,
-    html,
-  });
 };
